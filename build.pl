900169f9fef706571b88e22e20b6dcb2c97a6c9d
Steve Fischer <sfischer@upenn.edu>
2002-11-26 19:07:27 +0000

    Initial revision
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/install/trunk@2663 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ build.pl
@@ -0,0 +1,87 @@
+#########################################################################
+#########################################################################
+#
+# A convenience wrapper around ant.  Simplifies the input args.
+#
+#########################################################################
+#########################################################################
+
+
+use strict;
+use Cwd 'realpath'; 
+
+my @projects = ("AllGenes", "Annotator", "CBIL", "DJob", "DoTS", "GUS", "ParaDBs", "PlasmoDB");
+
+my @whats = ("install", "installweb", "package");
+
+my $projectHome = $ENV{PROJECT_HOME};
+
+if (!$projectHome) {
+  if (! (-e "build.pl" && -d "../install")) {
+    print "Error: Please either define the \$PROJECT_HOME environment variable or run install from the install/ directory\n";
+    exit 1;
+  }
+  $projectHome = realpath("..");
+} 
+
+my ($project, $component, $doWhat, $targetDir, $append, $doCheckout, $version) = &parseArgs(@ARGV);
+
+$| = 1;
+
+my $cmd = "ant -f $projectHome/install/build.xml $doWhat -Dproj=$project -DtargetDir=$targetDir -Dcomp=$component -DprojectsDir=$projectHome $append -logger org.apache.tools.ant.NoBannerLogger | grep ']'";
+
+print "\n$cmd\n\n";
+system($cmd);
+
+
+
+############################ subroutines ####################################
+
+sub parseArgs {
+
+    my $project = shift @ARGV;
+    my $component = "";
+    if ($project =~ /(\w+)(\/\w+)/ ) {
+	$project = $1;
+	$component = $2;
+    }
+    my $doWhat = shift @ARGV;
+    my $targetDir;
+    if ($ENV{GUS_HOME} && (!$ARGV[0] || $ARGV[0] =~ /^-/)) {
+	$targetDir = $ENV{GUS_HOME};
+    } else {
+	$targetDir = shift @ARGV;
+    }
+
+    &usage() unless $project && grep(/$project/, @projects);
+    &usage() unless $doWhat && grep(/$doWhat/, @whats);
+    &usage() unless $targetDir;
+
+
+    my ($append, $doCheckout, $version);
+    if ($ARGV[0] eq "-append") {
+	shift @ARGV;
+        $append = "-Dappend=true";
+    } 
+    if ($doCheckout = $ARGV[0]) {
+	&usage() if ($doCheckout ne "-co");
+	$version = $ARGV[1];
+    }
+
+    return ($project, $component, $doWhat, $targetDir, $append, $doCheckout, $version);
+}
+
+sub usage {
+    my $projects = join("|", @projects);
+    my $whats = join("|", @whats);
+
+    print 
+"
+usage: 
+  build $projects\[/componentname]  $whats  targetDir -append [-co [version]]
+
+";
+    exit 1;
+}
+
+
