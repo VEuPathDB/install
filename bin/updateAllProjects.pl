7a29f378f47de4844eea9de01612aab21e0444a5
Michael Saffitz <m@saffitz.com>
2005-06-16 20:29:29 +0000

    merged rel_3_5 to trunk
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/install/trunk@2921 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- bin/updateAllProjects.pl
+++
@@ -1,7 +0,0 @@
-foreach $dir (glob "$ENV{PROJECT_HOME}/*") {
-  next unless -d "$dir/CVS";
-  print STDERR "\nUpdating $dir\n";
-  chdir $dir;
-  system("cvs update -d") && die "\nFailed running cvs update -d in $dir. \n\nPerhaps you have a conflict in one of the files that was merged (look for a 'C' to the left of a file.  If so, edit that file, and search for '<<<<' which marks the start of the conflict(s))\n";
-  chdir $ENV{PROJECT_HOME};
-}
