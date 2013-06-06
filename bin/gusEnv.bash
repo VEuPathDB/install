08d495a010777efe4f61bc0ceb56800dc1d131fb
John Iodice <iodice@pennmedicine.upenn.edu>
2013-06-06 17:09:40 +0000

    add -local option to use the application directory containing the script, rather than the current $PWD, as the app dir
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/install/trunk@12278 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- bin/gusEnv.bash
+++ bin/gusEnv.bash
@@ -1,30 +1,53 @@
-# Set up the environment for developing a GUS application, using the current PWD
-# as the application directory (the parent to the gus_home and project_home
-# directories. It warns (but continues) if either directory does not exist. 
+# gusEnv.bash
+# Set up the environment for developing a GUS application.
 #
-# the script sets up:
-#  - $GUS_HOME
-#  - $PROJECT_HOME
-#  - $PATH
-#  - a prompt that shows the host, app name, and pwd
+# usage:
+#  $ source gusEnv.bash -local
+# or:
+#  $ cd my_app_dir
+#  $ source gusEnv.bash
+#
+# The script sets the following environment variables
+#  $PROJECT_HOME  - parent for Subversion project source-code directories
+#  $GUS_HOME  - parent for executables (installed largely from $PROJECT_HOME by build)
+#  $PATH  - bash's ordered list of directories to search for scripts run interactively
+#  $PS1  - bash's prompt string, set here to show the host, app name, and pwd
+#
+# It is assumed that the $PROJECT_HOME and $GUS_HOME are siblings, both under
+# the "application directory". If this script is run with the "-local" option,
+# it will use the application directory that contains it. For example,
+#
+# source /home/myApp/project_home/install/bin/gusEnv.bash -local
+#
+# will use /home/myApp as the application directory, setting PROJECT_HOME to
+# /home/myApp/project_home, and so forth. If "-local" is not specified, the
+# current $PWD is used as the application directory.
 #
 # To set $PATH, the script uses the environment variables $PRE_GUS_PATH and
 # $POST_GUS_PATH. The resulting path consists of GUS's binary directories
 # ($PROJECT_HOME/install/bin and $GUS_HOME/bin) preceeded by $PRE_GUS_PATH
 # and followed by $POST_GUS_PATH. If $POST_GUS_PATH is not defined, it is
 # set to the initial value of $PATH.
-#
-# usage:
-#  $ cd my_app_dir
-#  $ source gusEnv.bash
 
-if [ ! -d $PWD/project_home -a ! -L $PWD/project_home ]; then
-  echo "Error: directory '$PWD/project_home' does not exist"
+# default to PWD
+APP_DIR=$PWD
+
+# override with this script's app dir, if "-local" is specified
+if [ "$1" =  "-local" ]
+then
+  SCRIPT_DIR=`dirname $BASH_SOURCE`
+  APP_DIR=`cd ${SCRIPT_DIR}; cd ../../..; pwd`
+fi
+
+echo "[debug] \$APP_DIR=\"$APP_DIR\""
+
+if [ ! -d ${APP_DIR}/project_home -a ! -L ${APP_DIR}/project_home ]; then
+  echo "Error: directory '${APP_DIR}/project_home' does not exist"
 #  exit 1
 fi
 
-if [ ! -d $PWD/gus_home -a ! -L $PWD/gus_home ]; then
-  echo "Error: directory '$PWD/gus_home' does not exist"
+if [ ! -d ${APP_DIR}/gus_home -a ! -L ${APP_DIR}/gus_home ]; then
+  echo "Error: directory '${APP_DIR}/gus_home' does not exist"
 #  exit 1
 fi
 
@@ -33,11 +56,11 @@ if [ ! -n "$POST_GUS_PATH" ]; then
   export POST_GUS_PATH=$PATH
 fi
 
-export PROJECT_HOME=$PWD/project_home
-export GUS_HOME=$PWD/gus_home
+export PROJECT_HOME=${APP_DIR}/project_home
+export GUS_HOME=${APP_DIR}/gus_home
 export PATH=$PRE_GUS_PATH/:$PROJECT_HOME/install/bin:$GUS_HOME/bin:$POST_GUS_PATH
 
-pwd=`basename $PWD`
+pwd=`basename ${APP_DIR}`
 PS1="[\h:$pwd \W]$ "
 
 echo
