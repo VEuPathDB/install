872facfdb2a2efcc68736f20773886d3adef48e2
Bindu Gajria <bindu.gajria@gmail.com>
2016-08-22 14:28:56 +0000

    copy install/ from gus4 branch
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/install/trunk@18914 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ dashboard/view/databaseInfo.php
@@ -0,0 +1,267 @@
+<?php
+/**
+ * View of User and App database stats
+ * @package View
+ */
+
+require_once dirname(__FILE__) . "/../lib/modules/AppDatabase.php";
+require_once dirname(__FILE__) . "/../lib/modules/UserDatabase.php";
+require_once dirname(__FILE__) . "/../lib/modules/TuningManagerStatus.php";
+require_once dirname(__FILE__) . "/../lib/LdapTnsNameResolver.php";
+
+$app_database = new AppDatabase();
+$user_database = new UserDatabase();
+$ldap_resolver = new LdapTnsNameResolver();
+$tuning_manager_status = new TuningManagerStatus();
+
+if (isset($_GET['refresh']) && $_GET['refresh'] == 1) {
+  $success = $app_database->refresh();
+  // TODO - put this warning near the refresh button where it is better noticed
+  if ( ! $success) {print "<font color='red'>FAILED TO REFRESH</font>";}
+  $success = $user_database->refresh();
+  // TODO - put this warning near the refresh button where it is better noticed
+  if ( ! $success) {print "<font color='red'>FAILED TO REFRESH</font>";}
+}
+
+$adb = $app_database->attributes();
+$udb = $user_database->attributes();
+$adb_aliases_ar = $ldap_resolver->resolve($adb{'service_name'});
+$udb_aliases_ar = $ldap_resolver->resolve($udb{'service_name'});
+$tuning_status_attrs = $tuning_manager_status->attributes();
+
+?>
+<h2>Application Database</h2>
+<div class='related_dashboard_links'>
+Related Links
+<ul>
+<li>CBIL DBA Interface (password required)</li>
+  <ul>
+    <li><a href="https://www.cbil.upenn.edu/dba/uga.php">UGA databases</a></li>
+    <li><a href="https://www.cbil.upenn.edu/dba/">Penn databases</a></li>
+  </ul>
+</div>
+<p>
+<b>Service Name</b>:
+<?php print ($adb{'service_name'}) ? $adb{'service_name'} : "<span class='warn'>service name not available</span>"; ?>
+</p>
+
+<p>
+<b>Other Identifiers</b>:
+<table border="0" cellspacing="3" cellpadding="2" align="">
+<tr class="secondary3"><th><font size="-2">Identifier</font></th><th><font size="-2">Value</font></th><th></th></tr>
+<tr class="rowMedium"><td>Instance Name</td><td><?php print strtolower($adb{'instance_name'}) ?></td>
+    <td><a href='javascript:void()' style="text-decoration:none"
+        onmouseover="return overlib(
+         'result of <br><i>select&nbsp;sys_context(\'userenv\',&nbsp;\'instance_name\')&nbsp;from&nbsp;dual</i>'
+        )"
+        onmouseout = "return nd();"><sup>[?]</sup></a></td>
+</tr>
+<tr class="rowLight"><td>Global Name</td><td><?php print strtolower($adb{'global_name'}) ?></td>
+    <td><a href='javascript:void()' style="text-decoration:none"
+        onmouseover="return overlib(
+         'result of <br><i>select&nbsp;sys_context(\'userenv\',&nbsp;\'global_name\')&nbsp;from&nbsp;dual</i>'
+        )"
+        onmouseout = "return nd();"><sup>[?]</sup></a></td>
+</tr>
+<tr class="rowMedium"><td>DB Unique Name</td><td><?php print strtolower($adb{'db_unique_name'}) ?></td>
+    <td><a href='javascript:void()' style="text-decoration:none"
+        onmouseover="return overlib(
+         'result of <br><i>select&nbsp;sys_context(\'userenv\',&nbsp;\'db_unique_name\')&nbsp;from&nbsp;dual</i>'
+        )"
+        onmouseout = "return nd();"><sup>[?]</sup></a></td>
+</tr>
+</table>
+
+<br>
+
+<b>Aliases</b> (from LDAP): <?php print implode(", ", $adb_aliases_ar) ?>
+
+<br><br>
+<b>Hosted on</b>: <?php print strtolower($adb{'server_name'})?><br>
+<b>Oracle Version</b>: <?php print $adb{'version'}?>
+<p>
+<b>Client login name</b>: <?php print strtolower($adb{'login'})?><br>
+<b>Client connecting from</b>: <?php print strtolower($adb{'client_host'})?><br>
+<b>Client OS user</b>: <?php print strtolower($adb{'os_user'})?><br>
+<p>
+<b>Available DBLinks</b>:
+
+<table border="0" cellspacing="3" cellpadding="2" align="">
+
+<tr class="secondary3">
+<th align="left"><font size="-2">owner</font></th>
+<th align="left"><font size="-2">db_link</font></th>
+<th align="left"><font size="-2">username</font></th>
+<th align="left"><font size="-2">host</font></th>
+<th align="left"><font size="-2">created</font></th>
+</tr>
+<?php
+$dblink_map = $adb{'DblinkList'};
+$row = 0;
+foreach ($dblink_map as $dblink) {
+  $css_class = ($row % 2) ? "rowMedium" : "rowLight";
+?>
+<tr class="<?php print $css_class?>" >
+  <td><?php print strtolower($dblink{'owner'})?></td>
+  <td><?php print strtolower($dblink{'db_link'})?></td>
+  <td><?php print strtolower($dblink{'username'})?></td>
+  <td><?php print strtolower($dblink{'host'})?></td>
+  <td><?php print strtolower($dblink{'created'})?></td>
+</tr>
+<?php
+  $row++;
+}
+?>
+</table>
+
+
+<hr>
+<b>Information on this page was last updated</b>: <?php print $adb{'system_date'}?><br>
+<form method="GET" action="">
+<input name="refresh" type="hidden" value="1">
+<input type="submit" value="update now">
+</form>
+<p>
+
+
+<h2>Custom Tuning</h2>
+<p>
+
+<p class="clickable">Tuning Tables &#8593;&#8595;</p>
+<div class="expandable" style="padding: 5px;">
+
+<?php  $days_old_warning_threshold = 5; ?>
+
+<p>
+Color codes: <span class='fatal'>update failed</span>, 
+<span class='warn'>last_check older than <?php print $days_old_warning_threshold?> days</span>
+</p>
+<table border="0" cellspacing="3" cellpadding="2" align="">
+
+<tr class="secondary3">
+<th align="left"><font size="-2">name</font></th>
+<th align="left"><font size="-2">last_check</font></th>
+<th align="left"><font size="-2">status</font></th>
+<th align="left"><font size="-2">created</font></th>
+</tr>
+<?php
+$tm_status_map = $tuning_status_attrs{'table_statuses'};
+$row = 0;
+foreach ($tm_status_map as $table) {
+  $row_css_class = ($row % 2) ? "rowMedium" : "rowLight";
+  
+  $now = time();
+  $last_check_ts = strtotime($table{'last_check'});
+  $seconds_diff = $now - $last_check_ts;
+  $days_diff = $seconds_diff / 60 / 60 / 24;
+
+  if ($days_diff > $days_old_warning_threshold) {
+    $cell_css_class = "class='warn'";
+  } else if (stripos($table{'status'}, 'fail') !== FALSE) {
+    $cell_css_class =  "class='fatal'";
+  } else {
+    $cell_css_class = '';
+  }
+?>
+<tr class="<?php print $row_css_class?>" >
+  <td <?php print $cell_css_class?>><?php print $table{'name'}?></td>
+  <td <?php print $cell_css_class?>><?php print $table{'last_check'}?></td>
+  <td <?php print $cell_css_class?>><?php print $table{'status'}?></td>
+  <td <?php print $cell_css_class?>><?php print $table{'created'}?></td>
+</tr>
+<?php
+  $row++;
+}
+?>
+</table>
+
+</div>
+</p>
+
+
+
+<h2>WDK-Engine/Userlogin Database</h2>
+
+
+<p>
+<b>Identifiers</b>:
+<table border="0" cellspacing="3" cellpadding="2" align="">
+<tr class="secondary3"><th><font size="-2">Identifier</font></th><th><font size="-2">Value</font></th><th></th></tr>
+<tr class="rowLight"><td>Service Name</td><td><?php print strtolower($udb{'service_name'}) ?></td>
+    <td><a href='javascript:void()' style="text-decoration:none"
+        onmouseover="return overlib(
+         'result of <br><i>select&nbsp;sys_context(\'userenv\',&nbsp;\'service_name\')&nbsp;from&nbsp;dual</i>'
+        )"
+        onmouseout = "return nd();"><sup>[?]</sup></a></td>
+</tr>
+<tr class="rowMedium"><td>Instance Name</td><td><?php print strtolower($udb{'instance_name'}) ?></td>
+    <td><a href='javascript:void()' style="text-decoration:none"
+        onmouseover="return overlib(
+         'result of <br><i>select&nbsp;sys_context(\'userenv\',&nbsp;\'instance_name\')&nbsp;from&nbsp;dual</i>'
+        )"
+        onmouseout = "return nd();"><sup>[?]</sup></a></td>
+</tr>
+<tr class="rowLight"><td>Global Name</td><td><?php print strtolower($udb{'global_name'}) ?></td>
+    <td><a href='javascript:void()' style="text-decoration:none"
+        onmouseover="return overlib(
+         'result of <br><i>select&nbsp;sys_context(\'userenv\',&nbsp;\'global_name\')&nbsp;from&nbsp;dual</i>'
+        )"
+        onmouseout = "return nd();"><sup>[?]</sup></a></td>
+</tr>
+<tr class="rowMedium"><td>DB Unique Name</td><td><?php print strtolower($udb{'db_unique_name'}) ?></td>
+    <td><a href='javascript:void()' style="text-decoration:none"
+        onmouseover="return overlib(
+         'result of <br><i>select&nbsp;sys_context(\'userenv\',&nbsp;\'db_unique_name\')&nbsp;from&nbsp;dual</i>'
+        )"
+        onmouseout = "return nd();"><sup>[?]</sup></a></td>
+</tr>
+</table>
+<br>
+
+<b>Aliases</b> (from LDAP): <?php print implode(", ", $udb_aliases_ar) ?>
+
+<br><br>
+<b>Hosted on</b>: <?php print strtolower($udb{'server_name'}) ?><br>
+<b>Oracle Version</b>: <?php print $udb{'version'}?>
+<p>
+<b>Client login name</b>: <?php print strtolower($udb{'login'}) ?></b><br>
+<b>Client connecting from</b>: <?php print strtolower($udb{'client_host'})?><br>
+<b>Client OS user</b>: <?php print strtolower($udb{'os_user'})?><br>
+
+<p>
+<b>Available DBLinks</b>:
+
+<table border="0" cellspacing="3" cellpadding="2" align="">
+
+<tr class="secondary3">
+<th align="left"><font size="-2">owner</font></th>
+<th align="left"><font size="-2">db_link</font></th>
+<th align="left"><font size="-2">username</font></th>
+<th align="left"><font size="-2">host</font></th>
+<th align="left"><font size="-2">created</font></th>
+</tr>
+<?php
+$dblink_map = $udb{'DblinkList'};
+$row = 0;
+foreach ($dblink_map as $dblink) {
+  $css_class = ($row % 2) ? "rowMedium" : "rowLight";
+?>
+<tr class="<?php print $css_class?>" >
+  <td><?php print strtolower($dblink{'owner'})?></td>
+  <td><?php print strtolower($dblink{'db_link'})?></td>
+  <td><?php print strtolower($dblink{'username'})?></td>
+  <td><?php print strtolower($dblink{'host'})?></td>
+  <td><?php print strtolower($dblink{'created'})?></td>
+</tr>
+<?php
+  $row++;
+}
+?>
+</table>
+
+<hr>
+<b>Information on this page was last updated</b>: <?php print $udb{'system_date'}?><br>
+<form method="GET" action="">
+<input name="refresh" type="hidden" value="1">
+<input type="submit" value="update now">
+</form>
---
+++ dashboard/view/tomcatInfo.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Information about Tomcat and the Java VM.
+ * @package View
+ */
+
+require_once dirname(__FILE__) . "/../lib/modules/Jvm.php";
+require_once dirname(__FILE__) . "/../lib/modules/Webapp.php";
+require_once dirname(__FILE__) . "/../lib/modules/Catalina.php";
+
+$jvm = new Jvm();
+$webapp = new Webapp();
+$catalina = new Catalina();
+
+$jvm_data = $jvm->attributes();
+$webapp_data = $webapp->attributes();
+$catalina_data = $catalina->attributes();
+
+// TODO - if possible show undeployed or stopped instead of error for webapp uptime
+?>
+
+<h2>Tomcat</h2>
+
+
+<table class='p' border='0' cellpadding='0' cellspacing='0'>
+<tr><td><b>Instance:</b></td><td class="p"><?php print $jvm_data{'SystemProperties'}{'instance.name'} ?></td></tr>
+<tr><td><b>Instance uptime:</b></td><td class="p"><?php print $jvm->uptime_as_text() ?></td></tr>
+
+<tr><td>&nbsp;</td></tr>
+<tr><td><b>Webapp:</b> </td><td class="p"><?php print str_replace('/', '', $webapp_data{'path'}); ?></td></tr>
+
+<tr><td><b>Webapp uptime:</b></td><td class="p">
+<span id="webapp_uptime">
+  <?php $t=$webapp->uptime_as_text();  print (isset($t)) ? $t : "<span class='warn'>error</span>" ; ?>
+</span>
+</td></tr>
+<tr><td></td><td>
+  <button type="submit" id="reload_webapp" value="reload_webapp" onclick="reloadWebapp()">
+    Reload Webapp
+  </button>
+</td></tr>
+
+<tr><td>&nbsp;</td></tr>
+<tr><td><b>Servlet container:</b> </td><td class="p"><?php print $catalina_data{'ServerInfo'}; ?></td></tr>
+<tr><td><b>Servlet API version:</b> </td><td class="p"><?php print $catalina_data{'ServletApiVersion'}; ?></td></tr>
+<tr><td><b>JSP spec version:</b> </td><td class="p"><?php print $catalina_data{'JspSpecVersion'}; ?></td></tr>
+</table>
+<p>
+
+<p class="clickable">Webapp Classpath &#8593;&#8595;</p>
+<div class="expandable" style="padding: 5px;">
+<?php print str_replace(':', '<br>', $jvm_data{'ClassPath'}) ?><?php print str_replace(':', '<br>', $webapp_data{'loaderRepositoriesString'}) ?>
+</div>
+
+</p>
+<p>
+<b><a href="?p=Logger">Manage Log Levels</a></b> for this running instance of the webapp
+</p>
