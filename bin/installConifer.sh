ba64c678a6dcbd5dba0adbd84c3f7c0e6f450c9f
Ryan Doherty <rdoherty@upenn.edu>
2022-08-18 00:56:44 -0400

    Allow project_home's to contain projects for more than one cohort and still successfully build

--- bin/installConifer.sh
+++ bin/installConifer.sh
@@ -9,52 +9,66 @@
 #####################################################################
 
 # ensure correct number of arguments
-if (( $# != 2 )); then
-  >&2 echo "USAGE: installConifer.sh <gus_home> <project_home>"
+if (( $# != 3 )); then
+  >&2 echo "USAGE: installConifer.sh <gus_home> <project_home> <build_target>"
+  >&2 echo "     gus_home:     where scripts, code, and compiled artifacts will be placed"
+  >&2 echo "     project_home: location of GUS projects to be built"
+  >&2 echo "     build_target: top-level project submitted to bld/bldw/build (needed to determine cohort)"
   exit 1
 fi
 
 # name arguments
 gus_home=$1
 project_home=$2
+build_target=$3
 
-# check gus_home
+# log args
+echo "installConifer.sh called with: $gus_home $project_home $build_target"
+
+# check gus_home existence
 if ! [ -d "$gus_home" ]; then
   >&2 echo "ERROR: Missing directory for gus_home: $gus_home"
   exit 1
 fi
 
-# check project_home
+# check project_home existence
 if ! [ -d "$project_home" ]; then
   >&2 echo "ERROR: Missing directory for project_home: $project_home"
   exit 1
 fi
 
-# Define the supported cohorts based on presence of a certain project
-# Note: these must match the cohort mapping in the conifer script
-cohorts=( "ApiCommonWebsite:ApiCommon"
-          "OrthoMCLWebsite:OrthoMCL"
-          "ClinEpiWebsite:ClinEpi"
-          "MicrobiomeWebsite:Microbiome"
-          "WDKTemplateSite:WDKTemplate"
-          "EuPathDBIrods:EuPathDBIrods" )
+# check build target project existence
+if ! [ -d "$project_home/$build_target" ]; then
+  >&2 echo "ERROR: Requested build target $build_target does not exist in $project_home"
+  exit 1
+fi
 
-# search projects for one representing a single cohort; skip conifer if >1 or none
-for siteData in ${cohorts[@]}; do
-  map=( $(echo $siteData | sed 's/:/ /g') )
-  if [ -d "$project_home/${map[0]}" ]; then
-    if [ "$cohort" == "" ]; then
-      cohortRoot=${map[0]}
-      cohort=${map[1]}
-    else
-      >&2 echo "WARN: Skipping conifer installation.  More than one cohort root projectfound: $cohort, ${map[1]}"
-      exit 0
-    fi
+# Defines a mapping from:
+#    top level build project --> cohort name --> cohort root (for conifer)
+#
+# Note 1: the top-level build projects must match the cohort mapping in rebuilder
+# Note 2: the cohort names and roots must match the cohort mapping in the conifer script
+#
+cohorts=( "ApiCommonPresenters:ApiCommon:ApiCommonWebsite"
+          "OrthoMCLWebsite:OrthoMCL:OrthoMCLWebsite"
+          "ClinEpiPresenters:ClinEpi:ClinEpiWebsite"
+          "MicrobiomePresenters:Microbiome:MicrobiomeWebsite"
+          "WDKTemplateSite:WDKTemplate:WDKTemplateSite"
+          "EuPathDBIrods:EuPathDBIrods:EuPathDBIrods" )
+
+# search for cohort with one representing a single cohort; skip conifer if >1 or none
+for entry in ${cohorts[@]}; do
+  pair=( $(echo $entry | sed 's/:/ /g') )
+  if [ "$build_target" = "${pair[0]}" ]; then
+    cohort=${pair[1]}
+    cohortRoot=${pair[2]}
   fi
 done
 
 if [ "$cohort" == "" ]; then
-  >&2 echo "WARN: Skipping conifer installation.  No cohort root project found."
+  >&2 echo ""
+  >&2 echo "WARN: Skipping conifer installation.  Target project does not represent a conifer cohort's top level build project."
+  >&2 echo ""
   exit 0
 fi
 
